c9c89392dfd5a974734e2cef210139ead28c5963
Muhammad Luthfi Attaqi <114742143+tsimiscouse@users.noreply.github.com>
2024-11-26 22:58:14 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,7 @@ function App() {
     useEffect(() => {
         const checkAuth = async () => {
             try {
-                const response = await fetch('http://localhost:5000/api/auth/profile', {
+                const response = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/profile`, {
                     method: 'GET',
                     credentials: 'include',
                 });
